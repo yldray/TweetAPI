78b9c51f98f2409c886ebc64e5e74a27dd4f6964
Yıldıray TAŞBAŞ <yildirayt@idealdata.com.tr>
2022-02-22 13:40:26 +0300

    Add project files.

---
+++ TweetAt/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Models;
+
+namespace TweetAt
+{
+    public class Tweety
+    {
+        public async Task tweetConnectionAsync(string tweetString)
+        {
+            string APIKey = "-";
+            string APISecret = "-";
+            string APIToken = "-";
+            string APITokenSecret = "-";
+
+
+            var userClient = new TwitterClient(APIKey, APISecret, APIToken, APITokenSecret);
+            var user = await userClient.Users.GetAuthenticatedUserAsync();
+            Console.WriteLine(user);
+            var tweet = await userClient.Tweets.PublishTweetAsync("ssss");
+            Console.WriteLine("You published the tweet : ");
+        }
+    }
+}
